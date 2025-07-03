945d34cfdee512521a2ed0a5e771a0bd9df4a671
chrisitopherus <jochchristopher@gmail.com>
2025-07-03 22:23:11 +0200

    added full api documentation

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,17 @@
 // types
-export type { GuardFunc } from "./types/guard/guard";
+export type { GuardFunc, GuardFuncCollection } from "./types/guard/guard";
 export type { PipeFunc } from "./types/pipe/pipe";
 export type { PropertyName, Literal, Nullable, Nullish } from "./types/core";
+
+// components
+export { Guard } from "./guard/guard";
+export { GuardFactory } from './guard/guardFactory ';
+export { Pipe } from "./pipe/pipe";
+export { PipeBuilder } from "./pipe/pipeBuilder";
+export { Regex } from "./regex/regex";
+export { Transformation } from "./transformation/transformation";
+export { StringTransformation } from "./transformation/stringTransformation";
+export { NumberTransformation } from "./transformation/numberTransformation";
 
 // pickle
 export { Pickle } from "./pickle";
